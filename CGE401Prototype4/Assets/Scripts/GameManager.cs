b80649ef44516fadb71a0cf09010ed427d21e308
Mschoen03 <mschoen03a@gmail.com>
2025-11-15 15:27:13 -0600

    Done

---
+++ CGE401Prototype4/Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public TextMeshProUGUI waveText;
+    public TextMeshProUGUI messageText;
+
+    public bool playerLost = false;
+    public bool gameStarted = false;
+
+    private PlayerController player;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+
+        Time.timeScale = 0f;
+        messageText.text = "Press SPACE to Start!";
+    }
+
+    void Update()
+    {
+        if (!gameStarted)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                gameStarted = true;
+                Time.timeScale = 1f;
+                messageText.text = "";
+            }
+            return;
+        }
+
+        if (!playerLost && player.transform.position.y < -10)
+        {
+            PlayerLose();
+        }
+
+        if (playerLost && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void UpdateWaveUI(int wave)
+    {
+        waveText.text = "Wave: " + wave;
+    }
+
+    public void PlayerLose()
+    {
+        playerLost = true;
+        messageText.text = "YOU LOSE! Press R to Restart!";
+    }
+
+    public void PlayerWin()
+    {
+        playerLost = true;
+        messageText.text = "YOU WIN! Press R to Restart!";
+    }
+}
